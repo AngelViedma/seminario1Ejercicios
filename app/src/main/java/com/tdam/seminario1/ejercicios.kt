2ba2149efe13810ba40fe32753da3eb7c84d26f1
AngelViedma <19angelviedma@gmail.com>
2023-10-18 05:27:17 +0200

    ejercicio 13 falta por hacer

--- app/src/main/java/com/tdam/seminario1/ejercicios.kt
+++ app/src/main/java/com/tdam/seminario1/ejercicios.kt
@@ -9,6 +9,13 @@ fun main(){
     println(ejercicio5("Holaaaaa",'a'))
     ejercicio6("Hola me llamo pepe","23 me llamo")
     ejercicio7("hola me llamo angel")
+    ejercicio8(123)
+    print("El maximo comun divisor es: ")
+    println(ejercicio9(5,10))
+    print("La sucession de Fibonnaci es: ")
+    println(ejercicio10(4))
+    ejercicio11(6,35)
+    println(ejercicio12(101))
 }
 
 fun ejercicio1(lista: List<Int>):Int{
@@ -63,3 +70,58 @@ fun ejercicio7(texto:String){
     }
     println(textoNuevo)
 }
+
+fun ejercicio8(numero:Int){
+    var num_split=numero.toString().split("")
+    var num_list=num_split.toList()
+    var suma=0
+    for (num in 1 until num_list.size-1){
+        suma+=num
+    }
+    println("La suma de $numero es: $suma")
+}
+
+fun ejercicio9(a: Int, b: Int): Int{
+    return Math.max(a,b)
+}
+
+fun ejercicio10(n: Int): Int {
+    return if (n <= 1) {
+        n
+    } else {
+        ejercicio10(n - 1) + ejercicio10(n - 2)
+    }
+}
+
+fun ejercicio11(a: Int, b: Int){
+    if(ejercicio9(a,b)==1){
+        println("Son primos relativos")
+    }else{
+        println("No son primos relativos")
+    }
+}
+
+fun ejercicio12(numero: Int): Boolean {
+    var numeroInvertido = 0
+    var numeroOriginal = numero
+
+    while (numeroOriginal > 0) {
+        val digito = numeroOriginal % 10
+        numeroInvertido = numeroInvertido * 10 + digito
+        numeroOriginal /= 10
+    }
+
+    return numero == numeroInvertido
+}
+
+fun ejercicio13(cadena:String){
+
+    if(cadena.equals("a")){
+        println("<a></a>")
+
+    }else if (cadena.equals()){
+
+    }
+}
+
+

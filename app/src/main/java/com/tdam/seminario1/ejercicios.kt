200ebb373217c8ec8b02fdd040468977f74bf662
AngelViedma <19angelviedma@gmail.com>
2023-10-19 18:54:21 +0200

    ejercicio 28 falta

--- app/src/main/java/com/tdam/seminario1/ejercicios.kt
+++ app/src/main/java/com/tdam/seminario1/ejercicios.kt
@@ -1,6 +1,7 @@
 package com.tdam.seminario1
 
 import java.util.Locale
+import kotlin.math.max
 
 fun main(){
     val numeros= listOf(1,2,3,4,5)
@@ -19,9 +20,9 @@ fun main(){
     println(ejercicio10(4))
     ejercicio11(6,35)
     println(ejercicio12(101))
-    ejercicio13("p")
-    ejercicio13("p.coche#VWPolo")
-    ejercicio13("p.oferta")
+    ejercicio13("a")
+    ejercicio13("div.coche#VWPolo")
+    ejercicio13("div.oferta")
     ejercicio14(6)
     val num1= arrayOf(1,2,3,4,5)
     val num2= arrayOf(1,1,2,4,3)
@@ -29,7 +30,18 @@ fun main(){
     println(ejercicio20(5))
     println(ejercicio21("Hola"))
     println(ejercicio22(28))
-
+    println(ejercicio23(371,3))
+    val matriz = arrayOf(
+        arrayOf(12,2,3,4),
+        arrayOf(1,6,80,8),
+        arrayOf(9,10,11,12)
+    )
+    println(ejercicio24(matriz))
+    println(ejercicio25(matriz))
+    val listaStrings = listOf("hola","palabra","palabraMasLarga","xd")
+    println(ejercicio26(listaStrings))
+    println(ejercicio27(listaStrings))
+    println(ejercicio28("Hola9"))
 
 }
 
@@ -181,11 +193,78 @@ fun ejercicio21(cadena:String):String{
 }
 
 fun ejercicio22(numero:Int):Boolean{
-    return false
+    if (numero <= 0) {
+        return false
+    }
+    var sumaDivisores = 0
+
+    for (i in 1 until numero) {
+        if (numero % i == 0) {
+            sumaDivisores += i
+        }
+    }
+    return sumaDivisores == numero
+}
+//FALTA ARREGLAR LO DE LA POTENCIA
+fun ejercicio23(num:Int,potencia:Int):Boolean{
+    var numSplit=num.toString().split("")
+    var suma=0
+    for(i in 1 until numSplit.size-1){
+        suma+=numSplit[i].toInt()*potencia
+    }
+    return suma==num
 }
 
+fun ejercicio24(matriz:Array<Array<Int>>):Int{
+    var max=matriz[0][0]
 
+    for(i in matriz){
+        for(j in i){
+            if(j>max){
+                max=j
+            }
+        }
+    }
+    return max
+}
 
+fun ejercicio25(matriz:Array<Array<Int>>):Int{
+    var min=matriz[0][0]
+
+    for(i in matriz){
+        for (j in i){
+            if(j<min){
+                min=j
+            }
+        }
+    }
+    return min
+}
+
+fun ejercicio26(palabras:List<String>):String{
+    return palabras.maxByOrNull { it.length }.toString()
+}
+
+fun ejercicio27(palabra:List<String>):String{
+    var min=palabra[0]
+    for(i in palabra){
+        if(i.length<min.length){
+            min=i
+        }
+    }
+    return min
+}
+
+fun ejercicio28(cadena:String):Boolean{
+    var regex = """/^[0-9\s]*$/"""
+    return cadena.contains(regex)
+
+//    var aux = regex.toRegex().find(cadena)
+//
+//   // if(aux) return true
+//    return false
+
+}
 
 
 
